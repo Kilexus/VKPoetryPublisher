652fc424e492db1cb2345ed3c12649c5159cd197
Alexey Knorre <alexeyknorre@users.noreply.github.com>
2016-02-25 16:35:56 +0300

    Update post.py

--- post.py
+++ post.py
@@ -1,29 +1,39 @@
+# Hey, don't forget to fix parameters for random generator for yourself
+
 import vkontakte
-from time import gmtime, strftime
+import random
 
-publish=""
-store=""
+source="./store.txt"
 
-first_occur=False
-print "Retrieving brilliant poetry from database..."
-with open("./store.txt", 'r') as text:
-    for line in text:
-        if first_occur is False:
-            if "--------" in line:
-                first_occur=True
-            else:
-                publish=publish + line
-        else:
-            store = store + line
-
-with open("./store.txt", 'w') as text:
-    text.write(store)
+group_id = "YOUR_GROUP_ID"
+token = "YOUR_TOKEN" 
+#  Get it here: https://oauth.vk.com/authorize?client_id=YOUR_GROUP_ID&https://oauth.vk.com/blank.html&scope=wall,groups,offline&display=popup&response_type=token
 
 
-print "Pushing poetry to VK..."
+def pick_random_poem(source=source):
+    
+    print "Picking brilliant poetry from " + source
+    poem_number = random.randint(1,597)
+    count = 1
+    result=""
+    delimiter = "--------"
+    
+    with open(source, 'r') as text:
+        for line in text:
+            if count == poem_number and delimiter not in line:
+                result += line
+            if delimiter in line:
+                count += 1
+    return result.decode("cp1251")
+    
+def push_to_group(token=token,group_id=group_id):
+    
+    print "Pushing poetry to VK..."
+    vk = vkontakte.API(token=token)
+    publish = pick_random_poem()
+    
+    vk.wall.post(owner_id=group_id,friends_only=0,from_group=1,message=publish)
+    
+    print "Succesfully published"
 
-vk = vkontakte.API(token='YOUR_TOKEN')
-
-print vk.wall.post(owner_id=-#YOUR_GROUP_ID#,friends_only=0,from_group=1,message=publish.decode("cp1251"))
-
-print("Succesfully published.")
+push_to_group()
